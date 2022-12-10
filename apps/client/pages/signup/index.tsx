4d8b6c6130df4dd28ffd306c5cdd4a479b4855bc
VASAPOL RITTIDEAH <70092356+vasapol123@users.noreply.github.com>
2022-12-10 22:49:34 +0700

    feat: change contents and add styles on sign up page

--- apps/client/pages/signup/index.tsx
+++ apps/client/pages/signup/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { signIn } from 'next-auth/react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Signup as FormValues } from '@authentication/types';
 
+import styles from './signup.module.scss';
 import axios from '../../axios.config';
 import { signupSchema } from '../../validation/schema/signup.schema';
 
@@ -24,35 +26,38 @@ function Signup(): JSX.Element {
   };
 
   return (
-    <main>
-      <div>Sign Up</div>
-
-      <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <label htmlFor='email'>Email</label>
-          <input
-            {...register('email', { required: true })}
-            placeholder='email'
-          />
-          <label htmlFor='password'>Password</label>
-          <input
-            {...register('password', { required: true })}
-            type='password'
-            placeholder='password'
-          />
-          <label htmlFor='passwordConfirmation'>Password Confirmation</label>
-          <input
-            {...register('passwordConfirmation', { required: true })}
-            type='password'
-            placeholder='password confirmation'
-          />
-          <label htmlFor='displayName'>Display Name</label>
-          <input
-            {...register('displayName', { required: true })}
-            placeholder='display name'
-          />
-          <input type='submit' value='Submit' />
-        </form>
+    <main className={styles.container}>
+      <div className={styles.title}>Sign Up</div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...register('email', { required: true })}
+          placeholder='Email'
+          className={styles.form__email}
+        />
+        <input
+          {...register('password', { required: true })}
+          type='password'
+          placeholder='Password'
+          className={styles.form__password}
+        />
+        <input
+          {...register('passwordConfirmation', { required: true })}
+          type='password'
+          placeholder='Password Confirmation'
+          className={styles.form__passwordConfirmation}
+        />
+        <input
+          {...register('displayName', { required: true })}
+          placeholder='Display Name'
+          className={styles.form__displayName}
+        />
+        <input type='submit' value='Submit' className={styles.form__submit} />
+      </form>
+      <div className={styles.link}>
+        Already have an account?
+        <span onClick={() => signIn()} className={styles.link__signin}>
+          Sign in
+        </span>
       </div>
     </main>
   );
