99d65228f0880500ace2e1eeaa12ce02675cf3c6
VASAPOL RITTIDEAH <70092356+vasapol123@users.noreply.github.com>
2022-12-17 01:01:07 +0700

    refactor(testing): refactor user.service.spec to use createRandomUser()

--- apps/server/src/app/users/users.service.spec.ts
+++ apps/server/src/app/users/users.service.spec.ts
@@ -1,7 +1,9 @@
+import { faker } from '@faker-js/faker';
 import { ForbiddenException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Prisma, User } from '@prisma/client';
 
+import { createRandomUser } from '../../test/unit/fixtures/user.fixture';
 import { AppModule } from '../app.module';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -10,34 +12,16 @@ import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
-
-  const user: User = {
-    id: Date.now(),
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    email: 'jonathan@example.com',
-    displayName: 'Jonathan',
-    hashedPassword: 'fakeHashedPassword',
-    hashedRefreshToken: 'fakeRefreshToken',
-  };
-
-  const userArray: Array<User> = [user, user];
+  const fakeUser = createRandomUser();
+  const fakeUserArray: Array<User> = [createRandomUser(), createRandomUser()];
 
   const mockPrismaService = {
     user: {
-      create: jest
-        .fn()
-        .mockImplementation((user) =>
-          Promise.resolve({ id: Date.now(), ...user.data }),
-        ),
-      update: jest
-        .fn()
-        .mockImplementation((user) =>
-          Promise.resolve({ id: Date.now(), ...user.data }),
-        ),
-      findUnique: jest.fn().mockResolvedValue(user),
-      findMany: jest.fn().mockResolvedValue(userArray),
-      delete: jest.fn().mockResolvedValue(user),
+      create: jest.fn<Promise<User>, []>().mockResolvedValue(fakeUser),
+      update: jest.fn<Promise<User>, []>().mockResolvedValue(fakeUser),
+      findUnique: jest.fn<Promise<User>, []>().mockResolvedValue(fakeUser),
+      findMany: jest.fn<Promise<User[]>, []>().mockResolvedValue(fakeUserArray),
+      delete: jest.fn<Promise<User>, []>().mockResolvedValue(fakeUser),
     },
   };
 
@@ -52,80 +36,136 @@ describe('UsersService', () => {
     service = module.get<UsersService>(UsersService);
   });
 
+  afterEach(async () => {
+    jest.clearAllMocks();
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
   describe('createUser', () => {
     const createUserDto: CreateUserDto = {
-      email: 'william@example.com',
-      displayName: 'William',
-      hashedPassword: 'newHashedPassword',
+      email: fakeUser.email,
+      displayName: fakeUser.displayName,
+      hashedPassword: fakeUser.hashedPassword,
     };
 
-    it('should create a new user record and return that', () => {
-      expect(service.createUser(createUserDto)).resolves.toEqual({
-        id: expect.any(Number),
-        ...createUserDto,
-      });
+    it('should call PrismaService create with expected params', async () => {
+      await service.createUser(createUserDto);
 
-      expect(mockPrismaService.user.create).toHaveBeenCalled();
+      expect(mockPrismaService.user.create).toHaveBeenCalledWith({
+        data: {
+          email: expect.any(String),
+          displayName: expect.any(String),
+          hashedPassword: expect.any(String),
+        },
+      });
     });
 
-    it('should throw a ForbiddenException error', () => {
+    it('should create a new user record and return that', async () => {
+      await expect(service.createUser(createUserDto)).resolves.toEqual(
+        fakeUser,
+      );
+    });
+
+    it('should throw a ForbiddenException error if the email is exist', () => {
       jest.spyOn(mockPrismaService.user, 'create').mockRejectedValueOnce(
-        new Prisma.PrismaClientKnownRequestError('message', {
+        new Prisma.PrismaClientKnownRequestError('error message', {
           code: 'P2002',
           clientVersion: 'clientVersion',
         }),
       );
       expect(service.createUser(createUserDto)).rejects.toThrowError(
-        ForbiddenException,
+        new ForbiddenException(
+          'There is a unique constraint violation, a new user cannot be created with this email',
+        ),
       );
     });
   });
 
   describe('updateUser', () => {
-    it('should update a user and return the user', () => {
-      const updateUserDto: UpdateUserDto = {
-        id: Date.now(),
-        email: 'example3@test.com',
-      };
+    const updateUserDto: UpdateUserDto = {
+      email: fakeUser.email,
+    };
 
-      expect(service.updateUser(updateUserDto)).resolves.toEqual({
-        id: expect.any(Number),
-        email: updateUserDto.email,
+    it('should call PrismaService update with expected params', async () => {
+      await service.updateUser(fakeUser.id, updateUserDto);
+
+      expect(mockPrismaService.user.update).toHaveBeenCalledWith({
+        where: {
+          id: fakeUser.id,
+        },
+        data: {
+          email: fakeUser.email,
+        },
       });
-
-      expect(mockPrismaService.user.update).toHaveBeenCalledTimes(1);
     });
-  });
 
-  describe('findUserByEmail', () => {
-    it('should get a single user', () => {
-      expect(service.findUserByEmail(user.email)).resolves.toEqual(user);
+    it('should update a user and return the user', async () => {
+      await expect(
+        service.updateUser(fakeUser.id, updateUserDto),
+      ).resolves.toEqual(fakeUser);
     });
-  });
 
-  describe('findUserById', () => {
-    it('should get a single user', () => {
-      expect(service.findUserById(Date.now())).resolves.toEqual(user);
-    });
-  });
+    it('should throw a ForbiddenException error if updated fail', async () => {
+      jest.spyOn(mockPrismaService.user, 'update').mockRejectedValueOnce(
+        new Prisma.PrismaClientKnownRequestError('error message', {
+          code: 'P2025',
+          clientVersion: 'clientVersion',
+        }),
+      );
 
-  describe('findUsersByIds', () => {
-    it('should return an array of users', () => {
-      expect(service.findUsersByIds([Date.now(), Date.now()])).resolves.toEqual(
-        userArray,
+      await expect(
+        service.updateUser(fakeUser.id, updateUserDto),
+      ).rejects.toThrowError(
+        new ForbiddenException(
+          'An operation failed because user does not exist',
+        ),
       );
     });
   });
 
-  describe('deleteUser', () => {
-    it('should call the delete method once', async () => {
-      await service.deleteUser(Date.now());
+  describe('findUserByEmail', () => {
+    it('should get a single user', async () => {
+      await expect(service.findUserByEmail(fakeUser.email)).resolves.toEqual(
+        fakeUser,
+      );
+    });
+  });
 
-      expect(mockPrismaService.user.delete).toHaveBeenCalledTimes(1);
+  describe('findUserById', () => {
+    it('should get a single user', async () => {
+      await expect(service.findUserById(fakeUser.id)).resolves.toEqual(
+        fakeUser,
+      );
+    });
+  });
+
+  describe('findUsersByIds', () => {
+    it('should return an array of users', async () => {
+      await expect(
+        service.findUsersByIds([
+          faker.datatype.number(),
+          faker.datatype.number(),
+        ]),
+      ).resolves.toEqual(fakeUserArray);
+    });
+  });
+
+  describe('deleteUser', () => {
+    it('should call PrismaService delete with expected params', async () => {
+      await service.deleteUser(fakeUser.id);
+
+      expect(mockPrismaService.user.delete).toHaveBeenCalledWith({
+        where: {
+          id: fakeUser.id,
+        },
+      });
+    });
+
+    it('should delete a user and return the user', async () => {
+      await expect(service.deleteUser(fakeUser.id)).resolves.toEqual(fakeUser);
     });
   });
 });
